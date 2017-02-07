f5748770b5794df520cc06853596c0ff4f181cdf
SchyzoAsynchrone <le.schyzophrene.asynchrone@gmx.fr>
2017-02-08 00:54:57 +0100

    Added get_square method to Board

--- board.py
+++ board.py
@@ -19,3 +19,11 @@ class Board(object):
     def get_column(self, index):
         return [self.cells[(i,index)] for i in range(9)]
 
+    def get_square(self, n, m):
+        """n is the row number of the square and m is its column number"""
+        square = list()
+        for i in range(n*3, n*3 + 2):
+            for j in range(m*3, m*3 + 2):
+                square.append(self.cells[(i,j)])
+        return square
+                
